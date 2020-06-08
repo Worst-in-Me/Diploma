08e83188466feac3ce8a4616e40b667c7f87f6b7
Worst-in-Me <ivan_kostin00@mail.ru>
2020-06-08 16:11:14 +0300

    game

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 const bcrypt = require('bcryptjs');
-const token = require('jsonwebtoken');
 
 
 const userSchema = new Schema({
